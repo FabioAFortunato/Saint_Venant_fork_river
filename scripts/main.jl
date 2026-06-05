ae6ff2e8aad9e86e007329d1be3f5a8cccd485a5
Fabio Fortunato <fabioaugustofortunatofilho@gmail.com>
2026-06-05 17:08:01 -0300

    fiz uma função para rodar os testes e mudei o ponto inicial

--- scripts/main.jl
+++ scripts/main.jl
@@ -5,25 +5,20 @@ include("../src/BFGS_BOBYQA.jl")
 include("../src/BOBYQA_application.jl")
 include("../src/MADS_application.jl")
 
+function teste_todos(; t = 5.0, x = [0.1; 0.06])
 
-BFGS_BOBYQA_full_dim_problem(tmax = 5.0)
-BFGS_BOBYQA_two_dim_problem(tmax = 5.0)
-full_dim_problem(tmax = 5.0)
-two_dim_problem(tmax = 5.0)
-bobyqa_full_dim_problem(tmax = 5.0)
-bobyqa_two_dim_problem(tmax = 5.0)
-mads_full_dim_problem(tmax = 5.0)
-mads_two_dim_problem(tmax = 5.0)
+    BFGS_BOBYQA_full_dim_problem(tmax = t, x0 = collect(range(x[1], x[2], length = 101)))
+    BFGS_BOBYQA_two_dim_problem(tmax = t, x0 = x)
+    full_dim_problem(tmax = t, x0 = collect(range(x[1], x[2], length = 101)))
+    two_dim_problem(tmax = t, x0 = x)
+    bobyqa_full_dim_problem(tmax = t, x0 = collect(range(x[1], x[2], length = 101)))
+    bobyqa_two_dim_problem(tmax = t, x0 = x)
+    mads_full_dim_problem(tmax = t, x0 = collect(range(x[1], x[2], length = 101)))
+    mads_two_dim_problem(tmax = t, x0 = x)
+end
 
 
-BFGS_BOBYQA_full_dim_problem(tmax = 15.0)
-BFGS_BOBYQA_two_dim_problem(tmax = 15.0)
-full_dim_problem(tmax = 15.0)
-two_dim_problem(tmax = 15.0)
-bobyqa_full_dim_problem(tmax = 15.0)
-bobyqa_two_dim_problem(tmax = 15.0)
-mads_full_dim_problem(tmax = 15.0)
-mads_two_dim_problem(tmax = 15.0)
+
 
 
 
